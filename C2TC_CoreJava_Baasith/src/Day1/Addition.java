81e0a10cd6d634419b365b719d20941bd67ad7a9
Akshaya120408 <akshayanallasamy12@gmail.com>
2025-08-14 15:58:25 +0530

    Commit

---
+++ C2TC_CoreJava_Baasith/src/Day1/Addition.java
@@ -0,0 +1,16 @@
+package Day1;
+import java.util.Scanner;
+public class Addition {
+	public static void main(String[] args) {
+		int a;
+		int b;
+		int c;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the values: ");
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = a+b;
+		System.out.print(c);
+	}
+}
