a15be8474a45a8729f3cf3016978add167fa15de
Nivetha-ux <nive13114@gmail.com>
2025-08-22 09:49:27 +0530

    Commit

---
+++ C2TC_CoreJava_Assignments/src/Assignment3/Book.java
@@ -0,0 +1,24 @@
+package Assignment3;
+
+public class Book extends Item {
+    private String author;
+   
+    public Book(int id, String title, String author) {
+        super(id, title); // Call parent constructor
+        this.author = author;
+    }
+    
+    // Override displayInfo method
+    @Override
+    public void displayInfo() {
+        System.out.println("=== BOOK ===");
+        super.displayInfo(); // Call parent's displayInfo
+        System.out.println("Author: " + author);
+        System.out.println();
+    }
+    
+    // Getter for author
+    public String getAuthor() {
+        return author;
+    }
+}
